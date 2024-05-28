68ac62677568d01cca31666d85f0b4fe820944b1
Jan Philipp Hafer <anon_1plus1equals3@mailbox.org>
2024-05-29 00:28:34 +0200

    synchronize to zig upstream master

--- build.zig
+++ build.zig
@@ -9,9 +9,9 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     // public module usable outside of this build.zig
-    const sec = b.addModule("libsecproc", .{ .root_source_file = .{ .path = "sec.zig" } });
+    const sec = b.addModule("libsecproc", .{ .root_source_file = b.path("sec.zig") });
     // private module unusable outside of this build.zig
-    // const sec = b.createModule(.{ .root_source_file = .{ .path = "sec.zig" } });
+    // const sec = b.createModule(.{ .root_source_file = b.path("sec.zig") });
 
     if (native_os == .wasi) return;
     const test_step = b.step("test", "Run unit tests");
@@ -19,14 +19,14 @@ pub fn build(b: *std.Build) void {
     { // common
         const child = b.addExecutable(.{
             .name = "child_min",
-            .root_source_file = .{ .path = "test/common/child_min.zig" },
+            .root_source_file = b.path("test/common/child_min.zig"),
             .optimize = optimize,
             .target = target,
         });
         child.root_module.addImport("sec", sec);
         const main = b.addExecutable(.{
             .name = "main_min",
-            .root_source_file = .{ .path = "test/common/main_min.zig" },
+            .root_source_file = b.path("test/common/main_min.zig"),
             .optimize = optimize,
             .target = target,
         });
@@ -41,7 +41,7 @@ pub fn build(b: *std.Build) void {
     if (native_os == .windows) {
         const child = b.addExecutable(.{
             .name = "child_explicit_handle_inheritance",
-            .root_source_file = .{ .path = "test/win/child_explicit_handle_inheritance.zig" },
+            .root_source_file = b.path("test/win/child_explicit_handle_inheritance.zig"),
             .optimize = optimize,
             .target = target,
         });
@@ -50,7 +50,7 @@ pub fn build(b: *std.Build) void {
 
         const main = b.addExecutable(.{
             .name = "main_explicit_handle_inheritance",
-            .root_source_file = .{ .path = "test/win/main_explicit_handle_inheritance.zig" },
+            .root_source_file = b.path("test/win/main_explicit_handle_inheritance.zig"),
             .optimize = optimize,
             .target = target,
         });
@@ -72,7 +72,7 @@ pub fn build(b: *std.Build) void {
     if (native_os == .windows) {
         const child = b.addExecutable(.{
             .name = "child_win32k_mitigation",
-            .root_source_file = .{ .path = "test/win/child_win32k_mitigation.zig" },
+            .root_source_file = b.path("test/win/child_win32k_mitigation.zig"),
             .optimize = optimize,
             .target = target,
         });
@@ -81,7 +81,7 @@ pub fn build(b: *std.Build) void {
 
         const main = b.addExecutable(.{
             .name = "main_win32k_mitigation",
-            .root_source_file = .{ .path = "test/win/main_win32k_mitigation.zig" },
+            .root_source_file = b.path("test/win/main_win32k_mitigation.zig"),
             .optimize = optimize,
             .target = target,
         });
@@ -103,7 +103,7 @@ pub fn build(b: *std.Build) void {
     if (native_os == .windows) {
         const child = b.addExecutable(.{
             .name = "evildescendent_child_job_api",
-            .root_source_file = .{ .path = "test/win/child_job_api.zig" },
+            .root_source_file = b.path("test/win/child_job_api.zig"),
             .optimize = optimize,
             .target = target,
         });
@@ -112,7 +112,7 @@ pub fn build(b: *std.Build) void {
 
         const main = b.addExecutable(.{
             .name = "main_job_api",
-            .root_source_file = .{ .path = "test/win/main_job_api.zig" },
+            .root_source_file = b.path("test/win/main_job_api.zig"),
             .optimize = optimize,
             .target = target,
         });
@@ -158,7 +158,7 @@ pub fn build(b: *std.Build) void {
     if (native_os == .windows) {
         // const child = b.addExecutable(.{
         //     .name = "child_acl",
-        //     .root_source_file = .{ .path = "test/win/child_DCAL.zig" },
+        //     .root_source_file = b.path ( "test/win/child_DCAL.zig" ),
         //     .optimize = optimize,
         //     .target = target,
         // });
@@ -167,7 +167,7 @@ pub fn build(b: *std.Build) void {
 
         const main = b.addExecutable(.{
             .name = "main_acl",
-            .root_source_file = .{ .path = "test/win/main_ACL.zig" },
+            .root_source_file = b.path("test/win/main_ACL.zig"),
             .optimize = optimize,
             .target = target,
         });
